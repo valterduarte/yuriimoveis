8c513fd7538dead9f6e255f8fe20f515903c2101
Tais Duarte <taisrsduarte@gmail.com>
2026-03-30 22:52:37 -0300

    Add lib, hooks, utils to Tailwind content paths to fix dynamic class purging

--- nextjs/tailwind.config.js
+++ nextjs/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: [
     './app/**/*.{js,jsx,ts,tsx}',
     './components/**/*.{js,jsx,ts,tsx}',
+    './lib/**/*.{js,jsx,ts,tsx}',
+    './hooks/**/*.{js,jsx,ts,tsx}',
+    './utils/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {
